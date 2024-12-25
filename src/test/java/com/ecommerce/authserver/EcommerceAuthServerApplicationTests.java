c89a0bbb8528fbdc524c8ca0d6197c4a7e388d39
eduardobv <eduardobv@gmail.com>
2024-12-25 18:04:06 +0100

    initial version

---
+++ src/test/java/com/ecommerce/authserver/EcommerceAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
